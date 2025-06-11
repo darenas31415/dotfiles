053cafc5b20502cacc12cea64132b536bf75c4f0
David Arenas <darenas31415@users.noreply.github.com>
2025-06-11 09:46:00 +0100

    Add git rebase alias

--- terminal/custom.zsh
+++ terminal/custom.zsh
@@ -1,2 +1,7 @@
 # Avoid issues with `gpg` as installed via Homebrew.
 export GPG_TTY=`tty`
+
+gitrei() {
+  local ref=${1:-main}
+  git -c rebase.instructionFormat='%s%nexec GIT_COMMITTER_DATE="%cD" git commit --amend --no-edit' rebase -i $ref
+}
